d6c008ebddf77747cb1b442d029fbd292a80f0e4
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-21 13:08:15 -0600

    Use ds.paired_t, add html output

--- paired_t.py
+++ paired_t.py
@@ -41,20 +41,75 @@ import numpy as np
 
 
 def main():
+    filetypes = [("csv and feather files", ".csv .CSV .feather .FEATHER")]
+    path_in_title = "Select csv or feather file to read"
+    initialdir = Path(__file__).parent.resolve()
+    output_url = "paired_sample_t.html"
+    header_title = "Paired-sample t test"
+    header_id = "paired-sample-t-test"
     hypothesized_difference = 4
     significance_level = 0.05
-    df = pd.read_csv('data_paired_t.csv')
-    df_before_after = df[['before', 'after']]
-    difference_calc = df['after'] - df['before']
-    scipyresult = stats.ttest_rel(df['before'], df['after'])
-    if scipyresult.pvalue < significance_level:
-        print('Statistically significant. The test statistic =',
-              scipyresult.statistic.round(3),
-              '. The p value = ', scipyresult.pvalue.round(3)), '.'
-    else:
-        print('Not statistically significant. The test statistic =',
-              scipyresult.statistic.round(3),
-              '. The p value = ', scipyresult.pvalue.round(3)), '.'
+    colour = "#0077bb"
+    decimals = 16
+    width = 7
+    # path_in = ds.ask_open_file_name_path(
+    #     title=path_in_title,
+    #     initialdir=initialdir,
+    #     filetypes=filetypes
+    # )
+    # df = pd.read_csv('paired_t_data.csv')
+    start_time = time.perf_counter()
+    original_stdout = ds.html_begin(
+        output_url=output_url,
+        header_title=header_title,
+        header_id=header_id
+    )
+    ds.script_summary(
+        script_path=Path(__file__),
+        action="started at"
+    )
+    ds.style_graph()
+    series1 = pd.Series(
+        data=[
+            68, 76, 74, 71, 71, 72, 75, 83, 75, 74, 76, 77, 78, 75, 75, 84, 77,
+            69, 75, 65
+        ],
+        name="before"
+    )
+    series2 = pd.Series(
+        data=[
+            67, 77, 74, 74, 69, 70, 71, 77, 71, 74, 73, 68, 71, 72, 77, 80, 74,
+            73, 72, 62
+        ],
+        name="after"
+    )
+    results = ds.paired_t(
+        series1=series1,
+        series2=series2,
+        significance_level=significance_level
+    )
+    # if t_test_pvalue < significance_level:
+    #     print('Statistically significant. The test statistic =',
+    #           t_test_statisic.round(3),
+    #           '. The p value = ', t_test_pvalue.round(3)), '.'
+    # else:
+    #     print('Not statistically significant. The test statistic =',
+    #           t_test_statisic.round(3),
+    #           '. The p value = ', t_test_pvalue.round(3)), '.'
+    print(results)
+    stop_time = time.perf_counter()
+    ds.script_summary(
+        script_path=Path(__file__),
+        action="finished at"
+    )
+    ds.report_summary(
+        start_time=start_time,
+        stop_time=stop_time
+    )
+    ds.html_end(
+        original_stdout=original_stdout,
+        output_url=output_url
+    )
 
 
 if __name__ == "__main__":
