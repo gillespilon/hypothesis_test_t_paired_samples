c14f5641e8aada5fa145f56ef9250ddf0de6e399
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-22 17:00:41 -0600

    Create test_paired_t.py

---
+++ test_paired_t.py
@@ -0,0 +1,65 @@
+import warnings
+
+import datasense as ds
+import pandas as pd
+
+
+warnings.filterwarnings("ignore")
+series1_paired = pd.Series(
+    data=[
+        68, 76, 74, 71, 71, 72, 75, 83, 75, 74, 76, 77, 78, 75, 75, 84, 77,
+        69, 75, 65
+    ],
+    name="before"
+)
+series2_paired = pd.Series(
+    data=[
+        67, 77, 74, 74, 69, 70, 71, 77, 71, 74, 73, 68, 71, 72, 77, 80, 74,
+        73, 72, 62
+    ],
+    name="after"
+)
+
+
+def test_paired_t():
+    result = ds.paired_t(
+        series1=series1_paired,
+        series2=series2_paired,
+        significance_level=0.05,
+        alternative_hypothesis="two-sided",
+    )
+    # expected = (
+    # t statistic, t p value, t power,
+    # Shapiro-Wilk statistic, Shapiro-Wilk p value,
+    # Anderson-Darling test statistic, Anderson-Darling p value
+    # )
+    expected = (
+        -3.0234339882840073, 0.006989193823492975,
+        0.971856951713562, 0.7935157418251038,
+        0.33171651643374744, 0.692
+    )
+    assert result == expected
+    result = ds.paired_t(
+        series1=series1_paired,
+        series2=series2_paired,
+        significance_level=0.05,
+        alternative_hypothesis="less"
+    )
+    expected = (
+        -3.0234339882840073, 0.9965054030882535,
+        0.971856951713562, 0.7935157418251038,
+        0.33171651643374744, 0.692
+    )
+    assert result == expected
+    result = ds.paired_t(
+        series1=series1_paired,
+        series2=series2_paired,
+        significance_level=0.05,
+        alternative_hypothesis="greater"
+    )
+    expected = (
+        -3.0234339882840073, 0.0034945969117464873,
+        0.971856951713562, 0.7935157418251038,
+        0.33171651643374744, 0.692
+    )
+    assert result == expected
